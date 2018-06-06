bc061ecf31f42fceee7ab1a12c9c46f8e5851dd5
Christian Kleinboelting <kleinboelting@signal7.de>
2018-06-06 10:25:15 +0200

    Extract HomeController

---
+++ src/main/java/com/rbmhtechnology/apidocserver/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.rbmhtechnology.apidocserver.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/")
+  String home() {
+    return "home";
+  }
+}
